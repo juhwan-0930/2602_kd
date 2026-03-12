5a1e7c86a07d3f4cad86d128b43685cf2cb9a92c
juhwan-0930 <a01024496935@gmail.com>
2026-03-13 02:03:41 +0900

    init

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-console.clear()
+console.clear();
 
-$('.list-1 > a').hover(
+$(".list-1, .list-1-item").hover(
   function () {
-    $('.list-1 > a, .list-1-item').addClass('active');
+    $(".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils").addClass("active");
   },
   function () {
-    $('.list-1 > a, .list-1-item').removeClass('active');
-  }
+    $(".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils").removeClass("active");
+  },
 );
-
