04e9a6c2d3a1bd1fd78b850dd383edf2dc912ab7
juhwan0930 <a01024496935@gmail.com>
2026-03-16 10:29:36 +0900

    in

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ console.clear();
 
 $(".list-1, .list-1-item").hover(
   function () {
-    $(".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils").addClass("active");
+    $(
+      ".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils",
+    ).addClass("active");
   },
   function () {
-    $(".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils").removeClass("active");
+    $(
+      ".list-1 > a, .list-1-item, .header, .btn_link, .inner_list, .utils",
+    ).removeClass("active");
+  },
+);
+
+$(".list-2, .list-2-item").hover(
+  function () {
+    $(".list-2 a, .list-2-item").addClass("active");
+  },
+  function () {
+    $(".list-2 a, .list-2-item").removeClass("active");
   },
 );
